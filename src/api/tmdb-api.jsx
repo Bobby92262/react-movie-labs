f196027ff995fd891449df2e6dc69eea9668d850
Bobby92262 <bobbyfinn086@gmail.com>
2025-11-01 21:50:35 +0000

    Further updates to Actor popUp. -Added biography, applied color scheme, created new hook/api for getting details.

--- src/api/tmdb-api.jsx
+++ src/api/tmdb-api.jsx
@@ -159,7 +159,7 @@ export const getTrendingMovies = () => {
    });
   };
 
-  // Get top-rated movies -- Not used Yet
+  // Get top-rated movies
   export const getTopRatedMovies = () => {
     return fetch(
       "https://api.themoviedb.org/3/movie/top_rated?api_key=" +
@@ -184,6 +184,27 @@ export const getTrendingMovies = () => {
     const { id } = idPart;
     return fetch(
       `https://api.themoviedb.org/3/movie/${id}/credits?api_key=` +
+        import.meta.env.VITE_TMDB_KEY +
+        "&language=en-US"
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
+    // Get details of actors
+  export const getPersonDetails = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}?api_key=` +
         import.meta.env.VITE_TMDB_KEY +
         "&language=en-US"
     ).then( (response) => {
--- src/components/actorPopUp/actorPopUp.jsx
+++ src/components/actorPopUp/actorPopUp.jsx
@@ -1,18 +1,35 @@
 import {Dialog, DialogTitle, DialogContent, Typography, Box } from "@mui/material";
 import usePersonImages from "../../hooks/usePersonImages";
+import usePersonDetails from "../../hooks/usePersonDetails";
 
 const ActorPopUp = ({ actor, onClose }) => {
     const {data: images, isPending } = usePersonImages(actor.id);
+    const {data: details, isPending: detailsPending } = usePersonDetails(actor.id); 
 
     return (
         <Dialog open onClose={onClose}>
             <DialogTitle>{actor.name}</DialogTitle>
-            <DialogContent>
+            <DialogContent
+                sx={{
+                    backgroundColor: "background.paper",
+                    color: "text.primary",
+                    padding: "3",
+                }}>
+                {detailsPending ? (
+                    <Typography>Loading biography...</Typography>
+                ) : details?.biography ? (
+                    <Typography variant="body2" sx={{ marginBottom: 2}}>
+                        {details.biography}
+                    </Typography>
+                ) : (
+                    <Typography> No biography available.</Typography>
+                )}
+                
                 {isPending ? (
                     <Typography>Loading images...</Typography>
                 ) : images?.profiles?.length? (
                     <Box sx={{ display: "flex", gap: 2 }}>
-                        {images.profiles.map((img, i) => (
+                        {images.profiles.slice(0, 3).map((img, i) => (
                             <img
                                 key={i}
                                 src={`https://image.tmdb.org/t/p/w300/${img.file_path}`}
@@ -20,6 +37,7 @@ const ActorPopUp = ({ actor, onClose }) => {
                                 style={{ width: "150px", borderRadius: "8px" }}
                                 />
                         ))}
+
                     </Box>
                 ) : (
                     <Typography>No images available</Typography>
